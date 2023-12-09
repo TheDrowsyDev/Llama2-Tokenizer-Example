3d3581b076952055e1bdf766d1a166d82b97684b
TheDrowsyDev <vyperius117@gmail.com>
2023-12-09 17:59:29 -0500

    Finished code updates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Llama2-Tokenizer-Example
 Example code for Llama2 Tokenizer
+
+This repository contains the code for the Llama2 tokenizer example blog. 
+
+To get started, pull down the Llama2 tokenizer files locally and install the `transformers` package.
+
+Then, simply run the `main.py` file.
+
+For more information on how the tokenizer works and more detailed instructions, please refer to the linked blog:
+
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print(f"Llama2 tokenizer overview: {tokenizer}")
 print(f"Llama2 Vocabulary Size: {len(tokenizer.get_vocab().keys())}\n")
 print(f"{stdout_padding} End of Llama2 Tokenizer Details {stdout_padding}\n")
 
-# Verify token id's for Llama2 special tokens
+# Verify token IDs for Llama2 special tokens
 print(f"{stdout_padding} Llama2 Special Tokens {stdout_padding}\n")
 
 UNK = "<unk>" # Unknown token
@@ -22,23 +22,27 @@ BOS, EOS = "<s>", "</s>" # Begin of sequnece and end of sequence tokens
 special_tokens = [UNK, BOS, EOS]
 
 for token in special_tokens:
-    print(f'Token id for the special token {token}: {tokenizer.get_vocab()[token]}')
+    print(f'Token ID for the special token {token}: {tokenizer.get_vocab()[token]}')
     print(f'Encoded {token} becomes: {tokenizer.encode(token)}\n')
 
 print(f"{stdout_padding} End of Llama2 Special Tokens {stdout_padding}\n")
 
-# Verify token id's for Llama2 prompt tokens
-print(f"{stdout_padding} Llama2 Prompt Tokens {stdout_padding}\n")
+# Verify token IDs for Llama2 prompt symbols
+print(f"{stdout_padding} Llama2 Prompt Symbols {stdout_padding}\n")
 
-B_INST, E_INST = "[INST]", "[/INST]" # Begin of instruction and end of instruction tokens
-B_SYS, E_SYS = "<<SYS>>\n", "\n<<SYS>>\n\n" # Begin of system message and end of system message tokens
+B_INST, E_INST = "[INST]", "[/INST]" # Begin of instruction and end of instruction symbols
+B_SYS, E_SYS = "<<SYS>>\n", "\n<<SYS>>\n\n" # Begin of system message and end of system message symbols
 
-prompt_tokens = [B_INST, E_INST, B_SYS, E_SYS]
+prompt_symbols = [B_INST, E_INST, B_SYS, E_SYS]
 
-for token in prompt_tokens:
-    print(f'Encoded {repr(token)} becomes: {tokenizer.encode(token)}')
+for symbol in prompt_symbols:
+    encoded_symbol = tokenizer.encode(symbol)
+    print(f'Encoded {repr(symbol)} becomes: {encoded_symbol}')
+    
+    for token in encoded_symbol:
+        print(f"\tToken ID {token} --> {repr(tokenizer.decode(token))}")
 
-print(f"\n{stdout_padding} End of Llama2 Prompt Tokens {stdout_padding}\n")
+print(f"\n{stdout_padding} End of Llama2 Prompt Symbols {stdout_padding}\n")
 
 # Test tokenizer on a sentence
 print(f"{stdout_padding} Llama2 Tokenizer Sentence Example {stdout_padding}\n")
@@ -47,8 +51,8 @@ encoded_output = tokenizer.encode(sentence)
 print(f"Original sentence: {sentence}")
 print(f"Encoded sentence: {encoded_output}")
 
-# Verify what each token id correlates to
+# Verify what each token ID correlates to
 for token in encoded_output:
-    print(f"Token id {token} --> {tokenizer.decode(token)}")
+    print(f"Token ID {token} --> {tokenizer.decode(token)}")
 
 print(f"{stdout_padding} End of Llama2 Tokenizer Sentence Example {stdout_padding}\n")
